b954879a37543f10e5add1f25bedd8c8eff89ce2
Aryan-Kumar47 <kumarayan1012@gmail.com>
2024-06-30 17:25:44 +0530

    first commit

--- src/components/YourWeather.js
+++ src/components/YourWeather.js
@@ -5,40 +5,47 @@ import { FaCloud } from "react-icons/fa6";
 import { WiHumidity } from "react-icons/wi";
 
 export default function YourWeather({data}) {
+  console.log(data)
   const date = new Date()
   return (
     <div className='w-full flex justify-center items-start mt-10'>
       <div className='flex justify-center items-center flex-col md:w-[60%] w-[80%] bg-slate-300 px-4 py-10 shadow-md rounded-md'>
-        <div className='flex w-full'>
-          <p className='text-[3.5rem]'>{data?.name}</p>
-          <img className='w-10 h-10 object-cover' src={`https://flagcdn.com/144x108/${data?.sys?.country.toLowerCase()}.png`} alt='flag'/>
-        </div>
-        <div className='flex flex-col w-full'>
-          <FormattedDate date={date}/>
-        </div>
-        <div className='flex w-full justify-between sm:items-center items-start flex-col sm:flex-row'>
-          <div className='flex items-center justify-center'>
-            <div className='w-20 h-20'>
-              <img className='w-full h-full object-cover' src={`http://openweathermap.org/img/w/${data?.weather?.[0]?.icon}.png`} alt='temp'/>
+        {
+          data.name ? (
+            <>
+            <div className='flex w-full'>
+              <p className='text-[3.5rem]'>{data?.name}</p>
+              <img className='w-10 h-10 object-cover' src={`https://flagcdn.com/144x108/${data?.sys?.country.toLowerCase()}.png`} alt='flag'/>
             </div>
-            <h1 className='text-5xl'>{data?.main?.temp}</h1>
-            <TbTemperatureCelsius size={35} />
-          </div>
-          <div className='text-xl flex flex-col justify-start items-start'>
-            <div className='flex justify-start items-center gap-x-4'>
-              <BsWind size={25} className='text-blue-400'/>
-              <p>{`${data?.wind?.speed} m/s`}</p>
+            <div className='flex flex-col w-full'>
+              <FormattedDate date={date}/>
             </div>
-            <div className='flex justify-start items-center gap-x-4'>
-              <WiHumidity size={40} className='text-blue-400'/>
-              <p>{`${data?.main?.humidity} %`}</p>
+            <div className='flex w-full justify-between sm:items-center items-start flex-col sm:flex-row'>
+              <div className='flex items-center justify-center'>
+                <div className='w-20 h-20'>
+                  <img className='w-full h-full object-cover' src={`http://openweathermap.org/img/w/${data?.weather?.[0]?.icon}.png`} alt='temp'/>
+                </div>
+                <h1 className='text-5xl'>{data?.main?.temp}</h1>
+                <TbTemperatureCelsius size={35} />
+              </div>
+              <div className='text-xl flex flex-col justify-start items-start'>
+                <div className='flex justify-start items-center gap-x-4'>
+                  <BsWind size={25} className='text-blue-400'/>
+                  <p>{`${data?.wind?.speed} m/s`}</p>
+                </div>
+                <div className='flex justify-start items-center gap-x-4'>
+                  <WiHumidity size={40} className='text-blue-400'/>
+                  <p>{`${data?.main?.humidity} %`}</p>
+                </div>
+                <div className='flex justify-start items-center gap-x-4'>
+                  <FaCloud size={25} className='text-blue-400'/>
+                  <p>{`${data?.clouds?.all} %`}</p>
+                </div>
+              </div>
             </div>
-            <div className='flex justify-start items-center gap-x-4'>
-              <FaCloud size={25} className='text-blue-400'/>
-              <p>{`${data?.clouds?.all} %`}</p>
-            </div>
-          </div>
-        </div>
+            </>
+          ) : <div className='text-lg'>Not Found</div>
+        }
       </div>
     </div>
   )
